5b33d05d5c0c95f5d477b67dd056a03d92f71319
Luna ALves <lunaalves.pb@gmail.com>
2025-03-11 14:34:00 -0300

    retirada do mock

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/css/style.css";
-import "./mocks/axiosMock";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
